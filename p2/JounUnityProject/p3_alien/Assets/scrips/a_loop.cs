c55b34cf4da4fcb315cc658db68a8b7ee5aa4cff
mijs3 <mijs.tenhove@hotmail.com>
2017-04-13 23:40:00 +0200

    23:39
    13-4-2017

--- p2/JounUnityProject/p3_alien/Assets/scrips/a_loop.cs
+++ p2/JounUnityProject/p3_alien/Assets/scrips/a_loop.cs
@@ -6,78 +6,47 @@ public class a_loop : MonoBehaviour
 {
     public UIM_Talk uimtalk;
     public Animator anime;
-    public GameObject go;
     public float f;
-    public float f2;
     public bool stLoop;
-    private Transform tv;
-    public Canvas gesperkje; 
+    public bool retchs;
+    public Canvas gesperkje;
 
-    void Start ()
-    {  
+    void Start()
+    {
         anime.SetBool("loop", true);
         stLoop = true;
- //     tv = go.transform;
-        
     }
 
     void Update()
     {
+        if (transform.position.x > 4 &&  stLoop == true)
         {
-            if (stLoop == true)
-            {
-                transform.position = Vector3.MoveTowards(transform.position, (new Vector3(-4, 0.6f, -2)), f);
+            retchs = true;
+            transform.rotation = Quaternion.Euler(new Vector3(0, -90, 0));
+        }
+        if(transform.position.x < -4 && stLoop == true)
+        {
+            retchs = false;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+        }
+        //
 
-
-                //  transform.position = Vector3.MoveTowards(transform.position, (new Vector3(4, 0.6f, -2)), f);
-                //  
-                //  }
-                //     if (transform.position == (new Vector3(-4, 0.6f, -2)))
-                //    {
-
-                //  tv.Rotate(new Vector3(90,90, 20),Time.deltaTime);
-                // transform.rotation = Vector3.MoveTowards(transform.rotation (new Vector3(0, 90, 0 )));
-                //  transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
-                //  if (transform.Rotatoin == (new Vector3( 0,0,0))
-                //   {
-
-                // }
-                // transform.position = Vector3.MoveTowards(transform.position, (new Vector3(0, 180, 0)), Time.deltaTime * f2);
-                // if ( transform.rotation => (new Vector3(0, 90 ,0)))
-
-                //  transform.position = Vector3.MoveTowards(transform.position, (new Vector3(4, 0.6f, -2)), f);
-
-            }
-       
+        if(stLoop == true && retchs == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, (new Vector3(-4, 0.6f, -2)), f);
+        }
+        if (stLoop == true && retchs == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, (new Vector3(4, 0.6f, -2)), f);
+        }
         if (gesperkje.enabled == true)
         {
-            
+
             anime.SetBool("loop", false);
             stLoop = false;
             anime.SetBool("hand", true);
         }
-   // }
-   // public void loopterug()
-   // {
-    //    if (go.transform.position == (new Vector3(-4, 0.6f, -2)))
-    ///    {
-     //       print("val");
-     //   }
     }
+}
 
-  
-
-            //   if (transform.position == (new Vector3(-4, 0.6f, -2)))
-            //   {
-
-            //   }
-
-
-            //  }
-            //  public void StopAnimatie ()
-            //  {
-            //    stLoop = false;
-            //  }
-        }
-    }
        
