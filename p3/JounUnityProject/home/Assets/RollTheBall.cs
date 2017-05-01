09985a579ed1aeb413d62b9149013e07bbb6d1a4
mijs3 <mijs.tenhove@hotmail.com>
2017-05-01 10:30:31 +0200

    n.v.t
    .

---
+++ p3/JounUnityProject/home/Assets/RollTheBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollTheBall : MonoBehaviour {
+   
+    
+    public float speedH;
+    public float speedV;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+            speedH = Input.GetAxis ("Horizontal");
+
+        Vector3 v = (new Vector3(0, 0, speedH));
+
+        rb.AddForce(v * speedH);
+
+
+
+
+
+    }
+}
