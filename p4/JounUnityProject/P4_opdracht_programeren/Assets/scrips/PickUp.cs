0937913d00aed94e6957b25a67dd4409d1d0c979
mijs3 <mijs.tenhove@hotmail.com>
2017-06-23 13:53:45 +0200

    troep

---
+++ p4/JounUnityProject/P4_opdracht_programeren/Assets/scrips/PickUp.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+	public Slot slot;
+	public Sprite gm;
+	public Item itemS;
+	public Image imagePU;
+
+	public void Update()
+	{
+		imagePU.GetComponent<Image>().sprite = gm;
+		gm = itemS.sprite;
+	}
+
+	public void Dragg()
+	{
+		if (imagePU.GetComponent<Image>().sprite != null)
+		{
+			print("b");
+			slot.imageDrag.GetComponent<Image>().sprite= gm ;
+			slot.onOf.SetActive(true);
+		}
+	}
+
+	public void Droppen()
+	{
+
+
+	}
+	// waneer je gm oppakt dan destroy gm en full slot.image 
+
+}
