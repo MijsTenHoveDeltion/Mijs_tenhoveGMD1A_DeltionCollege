054325bc7fa171ac10c4db3bc31edd6a2beb6b04
mijs3 <mijs.tenhove@hotmail.com>
2017-09-12 15:02:19 +0200

    12-9-2017-15;02

---
+++ p4/JounUnityProject/fireboy_watergirl/Assets/skrips/Moving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+	public float speed;
+	public float jumpRange;
+
+	public void Update ()
+	{
+		Debug.DrawRay(transform.position, -transform.up * 100, Color.red);
+
+		float speedH = Input.GetAxis("Horizontal") * speed;
+		transform.Translate(speedH,0.0f, 0.0f );
+
+		//if(Ray => 0)
+			float speedUp = Input.GetAxis("Vertical") * speed;
+			transform.Translate(0.0f, speedUp,0.0f );
+	}
+
+
+}
