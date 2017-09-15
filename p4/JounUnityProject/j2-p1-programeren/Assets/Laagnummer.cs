a28b2576600f3530b05081f1a29644494ee66220
mijs3 <mijs.tenhove@hotmail.com>
2017-09-15 10:25:26 +0200

    15-9-2017-15:25

---
+++ p4/JounUnityProject/j2-p1-programeren/Assets/Laagnummer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Laagnummer : MonoBehaviour
+{
+	public List<int> ListInt = new List<int>();
+	public List<int> ListInt2 = new List<int>();
+
+	public void Start()
+	{
+		ListInt = GetLowestNumber(ListInt2);
+	}
+
+	public void GetLowestNumber(List<int> inde)
+	{
+		for (int i = 0; i < inde.Count; i++)
+		{
+
+			//andere for om te tjeken wat het laagste nummer is
+			
+		}
+	}
+	
+}
--- p4/JounUnityProject/j2-p1-programeren/Assets/Nummbers.cs
+++ p4/JounUnityProject/j2-p1-programeren/Assets/Nummbers.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class Nummbers : MonoBehaviour
 {
-	public List<int> list = new List<int>();
+	public List<int> Test2 = new List<int>();
 	public List<int> Test1 = new List<int>();
 
 	public void Start()
 	{
-		Test1 = GetOddNumbers(list);
+		Test1 = GetOddNumbers(Test2);
 	}
 
 	public List<int> GetOddNumbers (List<int> intje)
 	{
-		print("a");
-		for (int i = 0; i < list.Count; i++)
+		List<int> list = new List<int>();
+		for (int i = 0; i < intje.Count; i++)
 		{
-			print("b");
-			if (list[i] % 2 != 0)
+			
+			if (intje[i] % 2 != 0)
+				// % int kijkt of hij deelbaar is aan de int
 			{
-				list.Add(i);
-				print("hoi");
+				list.Add(intje[i]);
+				
 			}
 			
 		}
 		
-		return intje;
+		return list;
 	}
 }
