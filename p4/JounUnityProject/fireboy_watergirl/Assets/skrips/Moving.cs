e0632129afddfe474398610624548ac2c48dd12c
mijs3 <mijs.tenhove@hotmail.com>
2017-09-18 14:16:21 +0200

    18-9-2017-14:16

--- p4/JounUnityProject/fireboy_watergirl/Assets/skrips/Moving.cs
+++ p4/JounUnityProject/fireboy_watergirl/Assets/skrips/Moving.cs
@@ -6,17 +6,45 @@ public class Moving : MonoBehaviour
 {
 	public float speed;
 	public float jumpRange;
+	public bool jumping;
+	public GameObject gm;
+
+	public void Start()
+	{
+		jumping = false;
+	}
 
 	public void Update ()
 	{
 		Debug.DrawRay(transform.position, -transform.up * 100, Color.red);
+		RaycastHit hit;
+		Ray jumpRay = new Ray(transform.position, Vector3.down); 
 
 		float speedH = Input.GetAxis("Horizontal") * speed;
 		transform.Translate(speedH,0.0f, 0.0f );
 
-		//if(Ray => 0)
+		if  (jumping == true&&Physics.Raycast(jumpRay, out hit, jumpRange))
+		{
+			print("c");
 			float speedUp = Input.GetAxis("Vertical") * speed;
 			transform.Translate(0.0f, speedUp,0.0f );
+		}
+		
+			
+	}
+	public void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("floor"))
+		{
+			print("a"); 
+			jumping = true;
+		}
+		else
+		{
+			print("b");
+			jumping = false;
+		}
+
 	}
 
 
---
+++ p4/JounUnityProject/fireboy_watergirl/Assets/skrips/PickUP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUP : MonoBehaviour {
+
+	public bool boletje;
+	public GameObject gm;
+	private void Start()
+	{
+		boletje = true;
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gm)
+		{
+			boletje = false;
+			//other.gameObject.SetActive(false);
+		}
+	}
+}
