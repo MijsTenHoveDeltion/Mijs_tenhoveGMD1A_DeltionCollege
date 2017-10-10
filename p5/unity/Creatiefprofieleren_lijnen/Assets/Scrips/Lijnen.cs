0bffba7634ab05aa225b0c7d2c8be914f86b5d9f
mijs3 <mijs.tenhove@hotmail.com>
2017-10-10 14:20:38 +0200

    10-10-2017

--- p5/unity/Creatiefprofieleren_lijnen/Assets/Scrips/Lijnen.cs
+++ p5/unity/Creatiefprofieleren_lijnen/Assets/Scrips/Lijnen.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class Lijnen : MonoBehaviour
 {
-	 List<Vector3> particalPosision = new List<Vector3>();
-	
-	//List<LineRenderer> lineRenderers = new List<LineRenderer>();
-
+	  List<Vector3> particalPosision = new List<Vector3>();
 
 	public ParticleSystem pSystem;
 	ParticleSystem.Particle[] m_Particles;
@@ -21,10 +18,11 @@ public class Lijnen : MonoBehaviour
 
 	public int line;
 	public int pcount;
+	public int gelijk;
 
 	void Start()
 	{
-		lineRender.GetComponent<LineRenderer>();
+		//lineRender.GetComponent<LineRenderer>();
 		Material particleMaterial = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
 		gm = new GameObject("Particle System");
 		gm.transform.Rotate(-90, 0, 0);
@@ -36,61 +34,53 @@ public class Lijnen : MonoBehaviour
 		mainModule.startColor = Color.blue;
 		mainModule.startSize = startsize;
 		mainModule.maxParticles = maxPart;
-		mainModule.startLifetime = startLife;
+		mainModule.startSpeed = startLife;
+		mainModule.simulationSpace = ParticleSystemSimulationSpace.World;
 
 		if (m_Particles == null)
 		{
 			m_Particles = new ParticleSystem.Particle[maxPart];
 
 		}
-		//|| m_Particles.Length < maxPart
-
+	
 		var sh = pSystem.shape;
 		sh.shapeType = ParticleSystemShapeType.Sphere;
-
+		
 	}
-
-
 	public void LateUpdate()
 	{
 		
 		pSystem.GetParticles(m_Particles);
 		pcount = pSystem.particleCount;
 
-		if (pcount < 10  )
-			{
-
-			// elker keer pcount teld er een bij op maak een list new list aan en zet daar de v3 in
-				print("h");
-			}
 		for (int i = 0; i < pcount; i++)
 		{
-			
 			if (maxPart > 0 && m_Particles[i].position != new Vector3(0f, 0f, 0f))
 			{
-
 				Vector3 p1_position = m_Particles[i].position;
-				particalPosision.Add(m_Particles[i].position);
+				particalPosision.Add(p1_position);
 
-				for (int j = i; j < pSystem.particleCount; j++)
+
+
+				lineRender.positionCount = pcount;
+				gelijk = i;
+				lineRender.SetPosition(gelijk, p1_position);
+
+
+
+				if (particalPosision[i] != m_Particles[i].position)
 				{
-					if (maxPart > 0 && m_Particles[i].position != new Vector3(0f, 0f, 0f)){}
-
-					particalPosision.Add(m_Particles[j].position);
-
-						Vector3 p2_position = m_Particles[j].position;
-
-						lineRender.SetPosition(0, p1_position);
-						lineRender.SetPosition(1, p2_position);
+					particalPosision.RemoveAt(i);
 				}
+		
 			}
 		}
 	}
-	//				
-	//	lineRender.SetPosition(1, p2_position);
-	//float dis = Vector3.SqrMagnitude(p1_position - p2_position);
+}
+// maak new line renderer aan ga door de v3 maar dan random om de kanecties te maken 
+// als ditense groter is dan int dan break de line 
 
-	}
+
 
 
 
