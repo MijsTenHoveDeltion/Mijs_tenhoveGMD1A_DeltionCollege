ebde9415098ca1ba43e52c35b86cc8d5e05ca391
mijs3 <mijs.tenhove@hotmail.com>
2017-11-24 12:29:41 +0100

    fotos

---
+++ P6/programeren_p6/Assets/Script/BlokjeEI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlokjeEI : MonoBehaviour
+{
+	public enum Noord
+	{
+		n =  90,
+		o = -90,
+		z = 180,
+		w =  0
+	};
+
+	public Noord noord;
+	public GameObject gm;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+			if (Input.GetButtonDown("Fire1"))
+		    {
+			transform.rotation = Quaternion.Euler (new Vector3(0,0,(int)Noord.n));
+
+		    }
+	}
+
+	void Enum()
+	{
+
+	}
+
+
+}
