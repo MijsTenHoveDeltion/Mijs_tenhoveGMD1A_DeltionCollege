9b8c42e38bb8de0405eab85c5bb274e8c8b25764
mijs3 <mijs.tenhove@hotmail.com>
2017-12-01 16:24:56 +0100

    vrijdag

---
+++ p5/unity/protatype/character_costunising/Assets/Scipt/CollerLetter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollerLetter : MonoBehaviour {
+
+	public Text t1;
+	
+
+	public Color[] color;
+
+	public int whatCollor;
+
+
+	public void Update()
+	{
+		for (int i = 0; i < color.Length; i++)
+		{
+			if (whatCollor == i)
+			{
+				t1.color = color[i];
+			}
+		}
+	}
+	public void Colortext(int index)
+	{
+		whatCollor = index;
+	}
+}
