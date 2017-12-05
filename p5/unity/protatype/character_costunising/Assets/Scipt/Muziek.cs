37e5527365e7667dbdb88f13524b57e9d03a153d
mijs3 <mijs.tenhove@hotmail.com>
2017-12-05 15:21:30 +0100

    eind dag

---
+++ p5/unity/protatype/character_costunising/Assets/Scipt/Muziek.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Muziek : MonoBehaviour
+{
+	public GameObject m_MyAudioSource;
+
+	public bool m_Play;
+	bool m_ToggleChange;
+
+	//public Image on;
+	//public Image of;
+
+	void Update()
+	{
+		Doewat();
+		
+	}
+
+	void Doewat()
+	{
+		if (m_Play == true)
+		{
+			m_MyAudioSource.SetActive(true); 
+		}
+
+		if (m_Play == false)
+		{
+			m_MyAudioSource.SetActive(false);
+		}
+	}
+
+	public void Aan()
+	{
+
+		if (m_Play == true)
+		{
+			m_Play = false;
+			
+		}
+		else 
+		{
+			m_Play = true;
+
+		}
+	}
+}
