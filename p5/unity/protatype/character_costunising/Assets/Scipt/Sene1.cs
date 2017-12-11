89ad2d03b483f755b61fbeddb6da40d1e5ef3e17
mijs3 <mijs.tenhove@hotmail.com>
2017-12-11 19:21:10 +0100

    klaas?

---
+++ p5/unity/protatype/character_costunising/Assets/Scipt/Sene1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sene1 : MonoBehaviour
+{
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void StopGamr()
+	{
+		Application.Quit();
+
+	}
+
+}
