3b5ae7ee7793e16a69c0ff2cc1865a42f1e108bb
mijs3 <mijs.tenhove@hotmail.com>
2017-12-14 14:01:35 +0100

    12-12-2017

---
+++ P6/Unity/prototype/piano game/Assets/Scrips/Actifader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actifader : MonoBehaviour
+{
+	public string key;
+	public GameObject note;
+	public bool boo;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(key)&&boo)
+		{
+		
+			Destroy(note);
+		}
+	}
+	public void  OnCollisionEnter2D(Collision2D coll)
+	{	boo = true;
+		if (coll.gameObject.tag == "note")
+		{
+			note = coll.gameObject;
+			
+		}
+		
+	}
+
+	public void OnCollisionExit2D(Collision2D coll)
+	{
+		boo = false;
+	}
+
+}
+
+
+
