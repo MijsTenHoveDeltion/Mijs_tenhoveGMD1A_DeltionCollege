8b631ff165b8ecd2816b5cea8be2eff58e77e0b2
mijs3 <mijs.tenhove@hotmail.com>
2018-01-25 22:26:43 +0100

    laat

--- P6/Unity/development_/Assets/Code/Needs.cs
+++ P6/Unity/development_/Assets/Code/Needs.cs
@@ -28,7 +28,7 @@ public class Needs : MonoBehaviour
 	}
 
 	public void Movingto()
-	{
+	{// een forloop gaat door de list met sliders. dan kijkt hij of de sliders value minder of gelijk is aan 1. zo ja i gelijk aan intje 
 		for (int i = 0; i < sl.Count; i++)
 		{
 			if (sl[i].value <= 1)
@@ -41,8 +41,7 @@ public class Needs : MonoBehaviour
 	}
 
 	public void OnTriggerEnter(Collider other)
-	{
-		print("coll");
+	{// als het gameobject colide met het gameobject en als de slider h=niet helemaal vol is. dan "laat je weer op"
 		if (slider.value < slider.maxValue)
 		{
 			slider.value += getting * Time.deltaTime;
