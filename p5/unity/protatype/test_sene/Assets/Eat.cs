16955d1e966285a823746cc222933556d77d25f1
mijs3 <mijs.tenhove@hotmail.com>
2018-04-06 08:45:41 +0200

    6-4-2018

---
+++ p5/unity/protatype/test_sene/Assets/Eat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eat : Needs
+{
+	public int food;
+
+	public void Start()
+	{
+		HapHap();
+	}
+
+	public void HapHap()
+	{
+		Debug.Log("haphap");
+		//PrintBace();
+
+	}
+
+}
