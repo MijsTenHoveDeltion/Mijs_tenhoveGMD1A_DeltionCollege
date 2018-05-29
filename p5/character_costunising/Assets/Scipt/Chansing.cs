72d926e52def1a1f5169a14b071642462bcab858
mijs3 <mijs.tenhove@hotmail.com>
2018-05-29 09:40:53 +0200

    29-5-2018

---
+++ p5/character_costunising/Assets/Scipt/Chansing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chansing : MonoBehaviour
+{
+	public SpriteRenderer nose;
+	public SpriteRenderer part;
+	public Sprite[] option;
+	public int index;
+
+	// Use this for initialization
+	void Update ()
+	{
+		for (int  i = 0;  i < option.Length; i++)
+		{
+			if (i == index)
+			{
+				part.sprite = option[i];
+			}
+		}
+	}
+
+	public void Swap()
+	{
+		if (index < option.Length - 1)
+		{
+			index++;
+		}
+		else
+		{
+			index = 0;
+		}
+
+	}
+
+	public void SwapBack()
+	{
+		if (index < option.Length - 1)
+		{
+			index--;
+		}
+		else
+		{
+			index = 0;
+		}
+
+	}
+}
+
+
---
+++ p5/character_costunising/Assets/Scipt/Collor.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Collor : MonoBehaviour
+{
+
+	public SpriteRenderer hooft;
+	public SpriteRenderer armen;
+	public SpriteRenderer bennen;
+	public SpriteRenderer torso;
+	public SpriteRenderer neus1;
+	public Sprite neus2;
+
+
+	public Image sqere;
+
+
+	public Color white;
+	public Color pink;
+	public Color coffe;
+	public Color dark;
+	public Color green;
+
+	public int whatCollor ;
+
+	
+
+	public void Update()
+	{
+		if (whatCollor == 1)
+		{
+			hooft.color = white;
+			armen.color = white;
+			bennen.color = white;
+			torso.color = white;
+			sqere.color = white;
+			if (neus1.sprite != neus2)
+			{
+				neus1.color = white;
+			}
+		}
+		else if (whatCollor == 2)
+		{
+			hooft.color = pink;
+			armen.color = pink;
+			bennen.color = pink;
+			torso.color = pink;
+			sqere.color = pink;
+			if (neus1.sprite != neus2)
+			{
+				neus1.color = pink;
+			}
+		}
+		else if (whatCollor == 3)
+		{
+			hooft.color = coffe;
+			armen.color = coffe;
+			bennen.color = coffe;
+			torso.color = coffe;
+			sqere.color = coffe;
+			if (neus1.sprite != neus2)
+			{
+				neus1.color = coffe;
+			}
+		}
+		else if (whatCollor == 4 )
+		{
+			hooft.color = dark;
+			armen.color = dark;
+			bennen.color = dark;
+			torso.color = dark;
+			sqere.color = dark;
+			if (neus1.sprite != neus2)
+			{
+				neus1.color = dark;
+			}
+		}
+		else if (whatCollor == 5)
+		{
+			hooft.color = green;
+			armen.color = green;
+			bennen.color = green;
+			torso.color = green;
+			sqere.color = green;
+			if (neus1.sprite != neus2)
+			{
+				neus1.color = green;
+			}
+		}
+	}
+
+	
+
+
+
+	public void ColorWhite()
+	{
+		whatCollor = 1;
+	}
+	public void ColorPink()
+	{
+		whatCollor = 2;
+	}
+	public void ColorCoffe()
+	{
+		whatCollor = 3;
+	}
+	public void ColorDark()
+	{
+		whatCollor = 4;
+	}
+	public void ColorGreen()
+	{
+		whatCollor = 5;
+	}
+	
+}
