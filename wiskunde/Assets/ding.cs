31fa3a4faba376bde971ba4a8643045bca9fc8d3
mijs3 <mijs.tenhove@hotmail.com>
2018-06-01 08:56:19 +0200

    1-6-2018'

---
+++ wiskunde/Assets/ding.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ding : MonoBehaviour
+{
+	public float Damage;
+	public float mainDamage;
+	public float pBonus;
+	public float fBonus;
+	public float critMutiplier;
+	public float blokDamage;
+	public float aMutiplier;
+	public float generalDamageMultiplier;
+
+	public Text t;
+
+	// Update is called once per frame
+	public void Update ()
+	{
+		t.Text = Damage.ToString;
+		Formule();
+		print(Damage);
+	}
+
+	public float Formule()
+	{
+		Damage = ((mainDamage * (1 + pBonus) + fBonus) * critMutiplier - blokDamage) * aMutiplier * generalDamageMultiplier;
+		return Damage;
+	}
+}
