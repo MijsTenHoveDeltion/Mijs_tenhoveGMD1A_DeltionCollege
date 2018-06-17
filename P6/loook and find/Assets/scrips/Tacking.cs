d31043eb0deb9c7541759f5a0f0047c39782cdec
mijs3 <mijs.tenhove@hotmail.com>
2018-06-17 17:28:54 +0200

    ui

---
+++ P6/loook and find/Assets/scrips/Tacking.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tacking : MonoBehaviour
+{
+	public int kliks;
+	public KeyCode mouseKlik;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(mouseKlik))
+		{
+			if (kliks !=0)
+			{
+				kliks -= 1;
+
+			}
+			else
+			{
+				print("strop");
+			}
+			
+		}
+		
+	}
+}
--- P6/loook and find/Assets/scrips/kli.cs
+++ P6/loook and find/Assets/scrips/kli.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class kli : MonoBehaviour
 {
+	public bool boo;
+	public GameObject imagename;
+	public GameObject ob;
+	public Tacking tacking;
 
-	// Use this for initialization
-	void Start ()
+	public GameObject ps;
+
+	void Start()
+	{
+		ps.SetActive( false);
+		//ps.GetComponent<particleSystem>().enebleEmission = false;
+	}
+
+	public void OnMouseDown()
 	{
 		
+		Debug.Log(imagename);
+		Destroy(ob);
+		Destroy(imagename);
+		tacking.kliks += 1;
+		//ps.GetComponent<particleSystem>().enebleEmission = true;
+		ps.SetActive(true);
+		//Instantiate (splase, obplase, splase.rotation);
+		//Start(ps);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
