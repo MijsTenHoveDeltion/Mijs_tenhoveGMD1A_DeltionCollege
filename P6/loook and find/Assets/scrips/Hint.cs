18e35f23f42c6d0fdf70b77fc22a481f24d535db
mijs3 <mijs.tenhove@hotmail.com>
2018-06-21 22:00:31 +0200

    men

---
+++ P6/loook and find/Assets/scrips/Hint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hint : MonoBehaviour
+{
+	public SpriteRenderer gm;
+	public Color green;
+	public Color white;
+	//private SpriteRenderer spriteR;
+
+	// Use this for initialization
+	void Start()
+	{
+		//spriteR = gameObject.GetComponent<SpriteRenderer>();
+
+		gm.color = green;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+
+}
---
+++ P6/loook and find/Assets/scrips/Menus.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menus : MonoBehaviour
+{
+	public Color hover;
+	public Color white;
+	public SpriteRenderer sp;
+
+	public GameObject canvas;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void OnMouseOver()
+	{
+		sp.color = hover;
+		print("werkt");
+	}
+	public void OnMouseExit()
+	{
+		sp.color = white;
+		print("werkt");
+	}
+
+	public void reload()
+	{
+		SceneManager.LoadScene(0);
+
+	}
+
+	public void Play()
+	{
+		canvas.SetActive(false);
+		canvas.SetActive(true);
+	}
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
